1c552a71c2af2ff4ce0a0e7591c8774f2067bce9
Edvard Schreiner Sjøblom <edvard.schreiner.sjoeblom@gmail.com>
2016-03-03 15:11:44 +0100

    Skrev noe

---
+++ sqlscript.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Treningsokt (
+  oktID int unsigned AUTO_INCREMENT NOT NULL,
+  dato date,
+  varighet int,
+  PRIMARY KEY (oktID),
+);
+
+CREATE TABLE OvelserTreningsokt(
+  oktID_fk int NOT NULL,
+  ovelseID_fk int NOT NULL,
+  FOREIGN KEY(oktID_fk) REFERENCES Treningsokt(oktID) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(ovelseID_fk) REFERENCES Ovelse(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Ovelse (
+  id int unsigned AUTO_INCREMENT NOT NULL,
+  navn varchar(50),
+  beskrivelse varchar(255),
+  PRIMARY KEY (id),
+);
