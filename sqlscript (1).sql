965f0b25a96885c87d72cfe46f492d1d4fd2f6a1
Eirik Kaldahl <eakaldahl@gmail.com>
2016-03-09 15:56:54 +0100

    Oversatt hele fuckings scriptet. faen.

--- sqlscript (1).sql
+++ sqlscript (1).sql
@@ -1,131 +1,131 @@
-CREATE TABLE Treningsokt (
-  okt_id int unsigned AUTO_INCREMENT NOT NULL,
-  dato date,
-  varighet int,
-  PRIMARY KEY (okt_id)
+CREATE TABLE trainingSession (
+  session_id int unsigned AUTO_INCREMENT NOT NULL,
+  date date,
+  duration int,
+  PRIMARY KEY (session_id)
 );
 
 /* weak */
-CREATE TABLE Notat (
-  notat_id int unsigned NOT NULL PRIMARY KEY
-  REFERENCES Treningsokt(okt_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  personlig_form int(1),
-  presentasjon int(1),
-  treningsformal varchar(255),
+CREATE TABLE Notes (
+  note_id int unsigned NOT NULL PRIMARY KEY
+  REFERENCES trainingSession(session_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  personal_shape int(1),
+  preformance int(1),
+  exercisePurpose varchar(255),
   tips varchar(255)
 );
 
-CREATE TABLE Kategori (
-  kategori_id int unsigned AUTO_INCREMENT NOT NULL,
+CREATE TABLE Category (
+  category_id int unsigned AUTO_INCREMENT NOT NULL,
   type varchar(50),
-  PRIMARY KEY(kategori_id)
+  PRIMARY KEY(category_id)
 );
 
-CREATE TABLE Ovelse (
-  ovelse_id int unsigned NOT NULL AUTO_INCREMENT,
-  kategori_id_fk int unsigned,
-  navn varchar(50),
-  beskrivelse varchar(255),
-  PRIMARY KEY (ovelse_id),
-  FOREIGN KEY (kategori_id_fk) REFERENCES Kategori(kategori_id)
+CREATE TABLE Exercise (
+  exercise_id int unsigned NOT NULL AUTO_INCREMENT,
+  category_id_fk int unsigned,
+  name varchar(50),
+  description varchar(255),
+  PRIMARY KEY (exercise_id),
+  FOREIGN KEY (category_id_fk) REFERENCES Category(category_id)
   ON UPDATE CASCADE
   ON DELETE SET NULL
 );
 
-CREATE TABLE Maal (
-  maal_id int unsigned AUTO_INCREMENT NOT NULL,
-  ovelse_id_fk int unsigned,
-  innen_dato date,
-  PRIMARY KEY(maal_id)
+CREATE TABLE Goals (
+  goal_id int unsigned AUTO_INCREMENT NOT NULL,
+  exercise_id_fk int unsigned,
+  within_date date,
+  PRIMARY KEY(goal_id)
 );
 
-CREATE TABLE Resultat (
-  resultat_id int unsigned AUTO_INCREMENT NOT NULL,
-  ovelse_id_fk int unsigned NOT NULL,
-  treningsokt_id_fk int unsigned NOT NULL,
-  PRIMARY KEY(resultat_id),
-  FOREIGN KEY(ovelse_id_fk) REFERENCES Ovelse(ovelse_id),
-  FOREIGN KEY(treningsokt_id_fk) REFERENCES Treningsokt(okt_id)
+CREATE TABLE Result (
+  result_id int unsigned AUTO_INCREMENT NOT NULL,
+  exercise_id_fk int unsigned NOT NULL,
+  trainingSession_id_fk int unsigned NOT NULL,
+  PRIMARY KEY(result_id),
+  FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id),
+  FOREIGN KEY(trainingSession_id_fk) REFERENCES trainingSession(session_id)
 );
 
-CREATE TABLE Mal (
-  mal_id int unsigned AUTO_INCREMENT NOT NULL,
-  navn varchar(50),
-  ovelse_id_fk int unsigned,
-  maal_aktiv smallint,
-  PRIMARY KEY(mal_id),
-  FOREIGN KEY(ovelse_id_fk) REFERENCES Ovelse(ovelse_id) ON UPDATE CASCADE
+CREATE TABLE Templates (
+  template_id int unsigned AUTO_INCREMENT NOT NULL,
+  name varchar(50),
+  exercise_id_fk int unsigned,
+  template_active smallint,
+  PRIMARY KEY(template_id),
+  FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE
 );
 
 /* -------------- SPLEISETABELLER -------------- */
-CREATE TABLE Kan_erstatte_ovelse (
-  ovelse_id_fk int unsigned NOT NULL,
-  kan_erstatte_id_fk int unsigned NOT NULL,
-  FOREIGN KEY(ovelse_id_fk) REFERENCES Ovelse(ovelse_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(kan_erstatte_id_fk) REFERENCES Ovelse(ovelse_id) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE can_replace_exercise (
+  exercise_id_fk int unsigned NOT NULL,
+  can_replace_id_fk int unsigned NOT NULL,
+  FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(can_replace_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Kategori_tilhorer (
-  overkategori_id_fk int unsigned NOT NULL,
-  kategori_id_fk int unsigned NOT NULL,
-  FOREIGN KEY(overkategori_id_fk) REFERENCES Kategori(kategori_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(kategori_id_fk) REFERENCES Kategori(kategori_id) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE category_belongs (
+  parentCatergory_id_fk int unsigned NOT NULL,
+  category_id_fk int unsigned NOT NULL,
+  FOREIGN KEY(parentCategory_id_fk) REFERENCES Category(Category_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(category_id_fk) REFERENCES Category(category_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Ovelse_i_mal (
-  mal_id_fk int unsigned,
-  ovelse_id_fk int unsigned,
-  FOREIGN KEY(mal_id_fk) REFERENCES Mal(mal_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(ovelse_id_fk) REFERENCES Ovelse(ovelse_id) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE exercise_in_template (
+  template_id_fk int unsigned,
+  exercise_id_fk int unsigned,
+  FOREIGN KEY(template_id_fk) REFERENCES Templates(template_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Ovelser_i_treningsokt(
-  okt_id_fk int unsigned NOT NULL,
-  ovelse_id_fk int unsigned NOT NULL,
-  FOREIGN KEY(okt_id_fk) REFERENCES Treningsokt(okt_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(ovelse_id_fk) REFERENCES Ovelse(ovelse_id) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE Exercise_in_trainingSession(
+  session_id_fk int unsigned NOT NULL,
+  exercise_id_fk int unsigned NOT NULL,
+  FOREIGN KEY(session_id_fk) REFERENCES trainingSession(session_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 /* -------------- ARVETABELLER -------------- */
-CREATE TABLE Utendorstrening (
-  okt_id_fk int unsigned,
-  vaerforhold varchar(255),
+CREATE TABLE outdoor_training (
+  session_id_fk int unsigned,
+  weather_condtition varchar(255),
   temp decimal(3,1),
-  FOREIGN KEY (okt_id_fk) REFERENCES Treningsokt(okt_id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (session_id_fk) REFERENCES trainingSession(session_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Innendorstrening (
-  okt_id_fk int unsigned,
-  ventilasjon varchar(255),
-  ant_tilskuere int,
-  FOREIGN KEY(okt_id_fk) REFERENCES Treningsokt(okt_id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE indoor_training (
+  session_id_fk int unsigned,
+  ventilation varchar(255),
+  spectators int,
+  FOREIGN KEY(session_id_fk) REFERENCES trainingSession(session_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Styrke_kondisjon_til_resultat (
-  resultat_id_fk int unsigned,
-  belastning int,
+CREATE TABLE strength_conditon_to_result (
+  result_id_fk int unsigned,
+  weight int,
   reps int,
-  ant_sett int,
-  FOREIGN KEY(resultat_id_fk) REFERENCES Resultat(resultat_id) ON DELETE CASCADE ON UPDATE CASCADE
+  sets int,
+  FOREIGN KEY(result_id_fk) REFERENCES Result(result_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Utholdenhet_til_resultat (
-  resultat_id_fk int unsigned,
+CREATE TABLE endurance_to_result (
+  result_id_fk int unsigned,
   km decimal(10,2),
-  tid int,
-  FOREIGN KEY(resultat_id_fk) REFERENCES Resultat(resultat_id) ON DELETE CASCADE ON UPDATE CASCADE
+  time int,
+  FOREIGN KEY(result_id_fk) REFERENCES Result(result_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Styrke_kondisjon_til_maal (
-  maal_id_fk int unsigned,
-  belastning int,
+CREATE TABLE strength_condition_to_goals (
+  goal_id_fk int unsigned,
+  weight int,
   reps int,
-  FOREIGN KEY(maal_id_fk) REFERENCES Maal(maal_id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(goal_id_fk) REFERENCES Goals(goal_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Utholdenhet_til_maal (
-  maal_id_fk int unsigned,
+CREATE TABLE endurance_to_goals (
+  goal_id_fk int unsigned,
   km decimal(10,2),
-  tid int,
-  FOREIGN KEY(maal_id_fk) REFERENCES Maal(maal_id) ON DELETE CASCADE ON UPDATE CASCADE
+  time int,
+  FOREIGN KEY(goal_id_fk) REFERENCES Goals(goal_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
