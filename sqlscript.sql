1858d8484ec5a25bcddd6ba9e4396a227358ba29
Edvard Schreiner Sjøblom <edvard.schreiner.sjoeblom@gmail.com>
2016-03-09 16:05:39 +0100

    Fixa litt form4tting, og renamet en fil

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,4 +1,4 @@
-CREATE TABLE trainingSession (
+CREATE TABLE Training_session (
   session_id int unsigned AUTO_INCREMENT NOT NULL,
   date date,
   duration int,
@@ -10,8 +10,8 @@ CREATE TABLE Notes (
   note_id int unsigned NOT NULL PRIMARY KEY
   REFERENCES trainingSession(session_id) ON DELETE CASCADE ON UPDATE CASCADE,
   personal_shape int(1),
-  preformance int(1),
-  exercisePurpose varchar(255),
+  performance int(1),
+  exercise_purpose varchar(255),
   tips varchar(255)
 );
 
@@ -42,10 +42,10 @@ CREATE TABLE Goals (
 CREATE TABLE Result (
   result_id int unsigned AUTO_INCREMENT NOT NULL,
   exercise_id_fk int unsigned NOT NULL,
-  trainingSession_id_fk int unsigned NOT NULL,
+  training_session_id_fk int unsigned NOT NULL,
   PRIMARY KEY(result_id),
   FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id),
-  FOREIGN KEY(trainingSession_id_fk) REFERENCES trainingSession(session_id)
+  FOREIGN KEY(training_session_id_fk) REFERENCES Training_session(session_id)
 );
 
 CREATE TABLE Templates (
@@ -58,50 +58,50 @@ CREATE TABLE Templates (
 );
 
 /* -------------- SPLEISETABELLER -------------- */
-CREATE TABLE can_replace_exercise (
+CREATE TABLE Can_replace_exercise (
   exercise_id_fk int unsigned NOT NULL,
   can_replace_id_fk int unsigned NOT NULL,
   FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(can_replace_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE category_belongs (
-  parentCatergory_id_fk int unsigned NOT NULL,
+CREATE TABLE Category_belongs (
+  parent_catergory_id_fk int unsigned NOT NULL,
   category_id_fk int unsigned NOT NULL,
-  FOREIGN KEY(parentCategory_id_fk) REFERENCES Category(Category_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(parent_category_id_fk) REFERENCES Category(category_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(category_id_fk) REFERENCES Category(category_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE exercise_in_template (
+CREATE TABLE Exercise_in_template (
   template_id_fk int unsigned,
   exercise_id_fk int unsigned,
   FOREIGN KEY(template_id_fk) REFERENCES Templates(template_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Exercise_in_trainingSession(
+CREATE TABLE Exercise_in_training_session(
   session_id_fk int unsigned NOT NULL,
   exercise_id_fk int unsigned NOT NULL,
-  FOREIGN KEY(session_id_fk) REFERENCES trainingSession(session_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(session_id_fk) REFERENCES Training_session(session_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(exercise_id_fk) REFERENCES Exercise(exercise_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 /* -------------- ARVETABELLER -------------- */
-CREATE TABLE outdoor_training (
+CREATE TABLE Outdoor_training (
   session_id_fk int unsigned,
   weather_condtition varchar(255),
   temp decimal(3,1),
-  FOREIGN KEY (session_id_fk) REFERENCES trainingSession(session_id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (session_id_fk) REFERENCES Training_session(session_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE indoor_training (
+CREATE TABLE Indoor_training (
   session_id_fk int unsigned,
   ventilation varchar(255),
   spectators int,
-  FOREIGN KEY(session_id_fk) REFERENCES trainingSession(session_id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(session_id_fk) REFERENCES Training_session(session_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE strength_conditon_to_result (
+CREATE TABLE Strength_conditon_to_result (
   result_id_fk int unsigned,
   weight int,
   reps int,
@@ -109,21 +109,21 @@ CREATE TABLE strength_conditon_to_result (
   FOREIGN KEY(result_id_fk) REFERENCES Result(result_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE endurance_to_result (
+CREATE TABLE Endurance_to_result (
   result_id_fk int unsigned,
   km decimal(10,2),
   time int,
   FOREIGN KEY(result_id_fk) REFERENCES Result(result_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE strength_condition_to_goals (
+CREATE TABLE Strength_condition_to_goals (
   goal_id_fk int unsigned,
   weight int,
   reps int,
   FOREIGN KEY(goal_id_fk) REFERENCES Goals(goal_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE endurance_to_goals (
+CREATE TABLE Endurance_to_goals (
   goal_id_fk int unsigned,
   km decimal(10,2),
   time int,
