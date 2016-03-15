65b8011ee766e395610b6f2eb9014c1e2412d866
Edvard Schreiner Sjøblom <edvard.schreiner.sjoeblom@gmail.com>
2016-03-15 15:23:56 +0100

    added ability to store note in database

--- src/sample/core/Note.java
+++ src/sample/core/Note.java
@@ -1,20 +1,40 @@
 package sample.core;
 
+import sample.Main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Note {
     private int shape;
     private int performance;
     private String exercise_purpose;
     private String tips;
-    private int sessionid_fk;
+    private int session_id_fk;
+    protected Connection myConnection = Main.getDB().getConnection();
 
-    public Note(int shape, int performance, String exercise_purpose, String tips,int sessionid_fk){
+    public Note(int shape, int performance, String exercise_purpose, String tips,int session_id_fk){
         this.shape = shape;
         this.performance = performance;
         this.exercise_purpose = exercise_purpose;
         this.tips = tips;
-        this.sessionid_fk = sessionid_fk;
+        this.session_id_fk = session_id_fk;
     }
 
+    public void storeNote(){
+        try {
+            PreparedStatement ps = myConnection.prepareStatement("INSERT INTO Notes(session_id_fk,personal_shape,performance,exercise_purpose,tips) VALUES (?,?,?,?,?)");
+            ps.setInt(1,session_id_fk);
+            ps.setInt(2,shape);
+            ps.setInt(3,performance);
+            ps.setString(4,exercise_purpose);
+            ps.setString(5,tips);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     public String getTips() {
         return tips;
     }
@@ -46,4 +66,5 @@ public class Note {
     public void setExercise_purpose(String exercise_purpose) {
         this.exercise_purpose = exercise_purpose;
     }
+
 }
