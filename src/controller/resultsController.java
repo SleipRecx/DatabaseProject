36e5a1ed1ddd9c07a223832f082158774098fee2
Eirik Kaldahl <eakaldahl@gmail.com>
2016-03-15 16:51:44 +0100

    fix på result

--- src/controller/resultsController.java
+++ src/controller/resultsController.java
@@ -1,10 +1,7 @@
 package controller;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
 public class resultsController {
@@ -24,9 +21,40 @@ public class resultsController {
 
     public void initialize() {
 
+        ToggleGroup group = new ToggleGroup();
+        strengthButton.setToggleGroup(group);
+        enduranceButton.setToggleGroup(group);
+        strengthButton.setSelected(true);
+
+        enduranceButton.selectedProperty().addListener((obj,oldValue,newValue)->{
+            if(newValue) {
+                timeLabel.setVisible(true);
+                timeField.setVisible(true);
+                distanceLabel.setVisible(true);
+                distanceField.setVisible(true);
+                repsLabel.setVisible(false);
+                repsField.setVisible(false);
+                weigthField.setVisible(false);
+                weigthLabel.setVisible(false);
+            }else{
+                timeLabel.setVisible(false);
+                timeField.setVisible(false);
+                distanceLabel.setVisible(false);
+                distanceField.setVisible(false);
+                repsLabel.setVisible(true);
+                repsField.setVisible(true);
+                weigthField.setVisible(true);
+                weigthLabel.setVisible(true);
+            }
+        });
+    }
+
+    public void fetchSessions(){
+
+        
     }
 
     public void buttonPressed(ActionEvent actionEvent) {
-        
+
     }
 }
