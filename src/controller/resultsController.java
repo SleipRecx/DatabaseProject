bd663fc10a37b7bb8ce03a0b9792e827ff0a99b6
Markus Andresen <markua@stud.ntnu.no>
2016-03-15 16:24:18 +0100

    result view designed

--- src/controller/resultsController.java
+++ src/controller/resultsController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class resultsController {
+
+    public TextField timeField;
+    public TextField distanceField;
+    public Label timeLabel;
+    public Label distanceLabel;
+    public Text messageLable;
+    public Label repsLabel;
+    public Label weigthLabel;
+    public TextField repsField;
+    public TextField weigthField;
+    public RadioButton strengthButton;
+    public RadioButton enduranceButton;
+    public ComboBox sessionChoice;
+
+    public void buttonPressed(ActionEvent actionEvent) {
+
+    }
 }
